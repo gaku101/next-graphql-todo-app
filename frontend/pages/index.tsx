65f8d459dfee3f6c14cbaf921e9cb01d4dbba928
takehiro <takehiro-r133@hotmail.co.jp>
2022-06-21 18:06:40 +0900

    first commit

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Todo App - frontend</title>
+      </Head>
+      <main>
+        <h1>Todo App</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
